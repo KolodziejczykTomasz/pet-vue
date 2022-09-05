23f8586eadfa59ef2f49deac7a93cb78a3b820dd
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2022-09-05 19:02:06 +0200

    Added new component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,30 @@
 <template>
-
+       <header>
+      <nav class="nav__container">
+        <div class="nav__brand">
+          <img class="nav__brand--image" src="../assets/images/logo.svg" alt="logo" />
+        </div>
+        <div>
+          <ul id="menu">
+            <li><a class="nav-item" href="#mission">Mission</a></li>
+            <li><a class="nav-item" href="#product">About the product</a></li>
+            <li><a class="nav-item" href="#contact">Contact</a></li>
+          </ul>
+        </div>
+        <div class="nav__switch__language">
+          <div>pl</div>
+          <div>en</div>
+        </div>
+        <div class="burger__wrapper">
+          <button id="burger" class="burger" :class="{ active }"
+                  @click="active = !active"></button>
+        </div>
+      </nav>
+  </header>
 </template>
 
 <script>
+
 export default {
   name: 'Header'
 }
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,29 +1,7 @@
 <template>
   <div id="HomePage">
     <router-view/>
-    <div>
-      <header>
-        <nav class="nav__container">
-          <div class="nav__brand">
-            <img class="nav__brand--image" src="../assets/images/logo.svg" alt="logo" />
-          </div>
-          <div>
-            <ul id="menu">
-              <li><a class="nav-item" href="#mission">Mission</a></li>
-              <li><a class="nav-item" href="#product">About the product</a></li>
-              <li><a class="nav-item" href="#contact">Contact</a></li>
-            </ul>
-          </div>
-          <div class="nav__switch__language">
-            <div>pl</div>
-            <div>en</div>
-          </div>
-          <div class="burger__wrapper">
-            <button id="burger" class="burger" :class="{ active }"
-                    @click="active = !active"></button>
-          </div>
-        </nav>
-      </header></div>
+    <Header/>
     <main>
       <section id="ofert">
         <div class="hero__container">
@@ -389,8 +367,12 @@
   </div>
 </template>
 <script>
+import Header from '../components/Header'
+
 export default {
-  name: 'HomePage',
+  components: {
+    Header
+  },
   data () {
     return {
       isOpenModal: false,
